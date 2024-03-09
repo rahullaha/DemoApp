156730759c22f211683afd88d14bfecb98978010
rahullaha <rahulkumarlaha40@gmail.com>
2024-03-09 16:27:36 +0530

    project 2

---
+++ DemoApp/src/com/pack/TestApp.java
@@ -0,0 +1,12 @@
+package com.pack;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="RKL";
+		String s2 = " Technology";
+        System.out.println(s1+s2);
+	}
+
+}
